4b99e5c939f434dedbebc7e4abc2a4e130c25eef
Serteni <84300054+Serteni@users.noreply.github.com>
2022-09-25 03:02:45 +0700

    Update Init.py

--- CalcPython/modules/Init.py
+++ CalcPython/modules/Init.py
@@ -2,17 +2,38 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 
+def butPlus(num1, num2)
+    n = num1 + num2
+    return n
+
+def butMinus(num1, num2)
+    n = num1 + num2
+    return n
+
+def butMult(num1, num2)
+    n = num1 + num2
+    return n
+
+def butDiv(num1, num2)
+    n = num1 + num2
+    return n
+
 def Initialize():
 
     root = tk.Tk()
     root.geometry("350x220")
     root.title("Calc")
 
-    label1 = ttk.Label(root, text = "baza").place(x = 10, y = 10)
-    label2 = ttk.Label(root, text = "actions").place(x = 200, y = 10)
+    label1 = ttk.Label(root, text = "1st num").place(x = 10, y = 10)
+    label2 = ttk.Label(root, text = "2nd num").place(x = 10, y = 35)
 
-    entry1 = ttk.Entry(root, width = 15).place(x = 40, y = 10)
-    entry2 = ttk.Entry(root, width = 15).place(x = 40, y = 30)
+    buttonplus = ttk.Button(root, text = "+", width = 3).place(x = 280, y = 8)
+    buttonminus = ttk.Button(root, text = "-", width = 3).place(x = 310, y = 8)
+    buttonmult = ttk.Button(root, text = "*", width = 3).place(x = 280, y = 35)
+    buttondiv = ttk.Button(root, text = "/", width = 3).place(x = 310, y = 35)
+
+    entry1 = ttk.Entry(root, width = 15).place(x = 70, y = 10)
+    entry2 = ttk.Entry(root, width = 15).place(x = 70, y = 35)
     
     buttonex = ttk.Button(command = root.destroy, text = "Exit", width = 9).place(x = 280, y = 190)
 
