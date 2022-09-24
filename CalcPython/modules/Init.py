b4e348ecfb54f26043db9fcb2d49b9a3160487b9
Serteni <84300054+Serteni@users.noreply.github.com>
2022-09-25 03:11:43 +0700

    Init and gitignore update

--- CalcPython/modules/Init.py
+++ CalcPython/modules/Init.py
@@ -2,21 +2,17 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 
-def butPlus(num1, num2)
+def ButPlus(num1, num2)
     n = num1 + num2
-    return n
 
-def butMinus(num1, num2)
+def ButMinus(num1, num2)
     n = num1 + num2
-    return n
 
-def butMult(num1, num2)
+def ButMult(num1, num2)
     n = num1 + num2
-    return n
 
-def butDiv(num1, num2)
+def ButDiv(num1, num2)
     n = num1 + num2
-    return n
 
 def Initialize():
 
@@ -27,14 +23,19 @@ def Initialize():
     label1 = ttk.Label(root, text = "1st num").place(x = 10, y = 10)
     label2 = ttk.Label(root, text = "2nd num").place(x = 10, y = 35)
 
-    buttonplus = ttk.Button(root, text = "+", width = 3).place(x = 280, y = 8)
-    buttonminus = ttk.Button(root, text = "-", width = 3).place(x = 310, y = 8)
-    buttonmult = ttk.Button(root, text = "*", width = 3).place(x = 280, y = 35)
-    buttondiv = ttk.Button(root, text = "/", width = 3).place(x = 310, y = 35)
-
     entry1 = ttk.Entry(root, width = 15).place(x = 70, y = 10)
     entry2 = ttk.Entry(root, width = 15).place(x = 70, y = 35)
-    
+
+    entry10 = ttk.Entry(root, width = 15).place(x = 70, y = 80)
+    entry11 = ttk.Entry(root, width = 15).place(x = 70, y = 105)
+    entry12 = ttk.Entry(root, width = 15).place(x = 70, y = 130)
+    entry13 = ttk.Entry(root, width = 15).place(x = 70, y = 155)s
+
+    buttonplus = ttk.Button(root, text = "+", width = 3, command = ButPlus(entry1.get(), entry2.get())).place(x = 280, y = 8)
+    buttonminus = ttk.Button(root, text = "-", width = 3, command = ButMinus(entry1.get(), entry2.get())).place(x = 310, y = 8)
+    buttonmult = ttk.Button(root, text = "*", width = 3, command = ButMult(entry1.get(), entry2.get())).place(x = 280, y = 35)
+    buttondiv = ttk.Button(root, text = "/", width = 3, command = ButDiv(entry1.get(), entry2.get())).place(x = 310, y = 35)
+
     buttonex = ttk.Button(command = root.destroy, text = "Exit", width = 9).place(x = 280, y = 190)
 
     root.mainloop()
